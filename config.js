dfff3d6a7ebc5688125d1a495950f85e5e4e273e
Mikael Owen <mikaelowen@Mikaels-MacBook-Air.local>
2024-10-05 00:16:08 +0800

    connect wallet function

---
+++ config.js
@@ -0,0 +1,19 @@
+import { http, createConfig } from 'wagmi'
+import { base, mainnet, optimism } from 'wagmi/chains'
+import { injected, metaMask, safe, walletConnect } from 'wagmi/connectors'
+
+const projectId = 'a37743b17ea2878a673d4ea37671f149'
+
+export const config = createConfig({
+  chains: [mainnet, base],
+  connectors: [
+    injected(),
+    walletConnect({ projectId }),
+    metaMask(),
+    safe(),
+  ],
+  transports: {
+    [mainnet.id]: http(),
+    [base.id]: http(),
+  },
+})
